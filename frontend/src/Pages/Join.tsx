8a26571af520dd39f809ed12e1be62aa43492233
성민 <tjdals14798@naver.com>
2024-12-16 22:36:40 +0900

    title update

--- frontend/src/Pages/Join.tsx
+++ frontend/src/Pages/Join.tsx
@@ -1,59 +1,98 @@
-import { FormEvent, useState } from 'react'
-import dayjs from 'dayjs'
+import { FormEvent, useState } from "react";
+import dayjs from "dayjs";
 
 const Join: React.FC = () => {
-
-  const [joinId, setJoinId] = useState<string>('');
-  const [joinPw, setJoinPw] = useState<string>('');
-  const [joinName, setJoinName] = useState<string>('');
-  const [joinDate, setJoinDate] = useState<string> ('');
+  const [joinId, setJoinId] = useState<string>("");
+  const [joinPw, setJoinPw] = useState<string>("");
+  const [joinName, setJoinName] = useState<string>("");
+  const [joinDate, setJoinDate] = useState<string>("");
 
   const handleJoin = (e: FormEvent): void => {
-      e.preventDefault();
-      alert(`가입 성공!\n아이디: ${joinId}\n이름: ${joinName}\n생년월일: ${joinDate}`);
-    };
+    e.preventDefault();
+    alert(
+      `가입 성공!\n아이디: ${joinId}\n이름: ${joinName}\n생년월일: ${joinDate}`
+    );
+  };
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-50 lg:px-8">
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
         <div className="py-6">
-            <h1 className="text-3xl font-bold tracking-tight text-gray-900">회원가입</h1>
+          <h1 className="text-3xl font-bold tracking-tight text-gray-900">
+            회원가입
+          </h1>
         </div>
         <form method="POST" className="space-y-6" onSubmit={handleJoin}>
-            <div className="mt-2">
-                <input id="joinId" name="joinId" type="text" required autoComplete="username" placeholder='아이디' value={joinId}
-                  onChange={(e) => setJoinId(e.target.value)}
-                  className="block w-full h-12 px-3 py-2 text-base text-gray-900 placeholder-gray-400 border border-gray-300 focus:z-10 focus:outline-indigo-600 focus:ring-indigo-600 sm:text-sm rounded-t-md"
-                />
-                <input id="joinPw" name="joinPw" type="password" required autoComplete="current-password" placeholder='비밀번호' value={joinPw}
-                  onChange={(e) => {setJoinPw(e.target.value)}}
-                  className="block w-full h-12 px-3 py-2 text-base text-gray-900 placeholder-gray-400 border border-gray-300 focus:z-10 focus:outline-indigo-600 focus:ring-indigo-600 sm:text-sm border-t-0 rounded-b-md"
-                />
-            </div>
+          <div className="mt-2">
+            <input
+              id="joinId"
+              name="joinId"
+              type="text"
+              required
+              autoComplete="username"
+              placeholder="아이디"
+              value={joinId}
+              onChange={(e) => setJoinId(e.target.value)}
+              className="block w-full h-12 px-3 py-2 text-base text-gray-900 placeholder-gray-400 border border-gray-300 focus:z-10 focus:outline-indigo-600 focus:ring-indigo-600 sm:text-sm rounded-t-md"
+            />
+            <input
+              id="joinPw"
+              name="joinPw"
+              type="password"
+              required
+              autoComplete="current-password"
+              placeholder="비밀번호"
+              value={joinPw}
+              onChange={(e) => {
+                setJoinPw(e.target.value);
+              }}
+              className="block w-full h-12 px-3 py-2 text-base text-gray-900 placeholder-gray-400 border border-gray-300 focus:z-10 focus:outline-indigo-600 focus:ring-indigo-600 sm:text-sm border-t-0 rounded-b-md"
+            />
+          </div>
 
-            <div className="mt-2">
-                <input id="joinName" name="joinName" type="text" required autoComplete="name" placeholder='이름' value={joinName}
-                  onChange={(e) => {setJoinName(e.target.value)}}
-                  className="block w-full h-12 px-3 py-2 text-base text-gray-900 placeholder-gray-400 border border-gray-300 focus:z-10 focus:outline-indigo-600 focus:ring-indigo-600 sm:text-sm rounded-t-md"
-                />
-                <input id="JoinDate" name="JoinDate" type="date" min="1950-01-01" max={dayjs().format("YYYY-MM-DD")} // 오늘 날짜까지
-                  required autoComplete="bday" placeholder='생년월일' value={joinDate}
-                  onChange={(e) => {setJoinDate(e.target.value)}}
-                  className="block w-full h-12 px-3 py-2 text-base text-gray-900 placeholder-gray-400 border border-gray-300 focus:z-10 focus:outline-indigo-600 focus:ring-indigo-600 sm:text-sm border-t-0 rounded-b-md"
-                />
-            </div>
+          <div className="mt-2">
+            <input
+              id="joinName"
+              name="joinName"
+              type="text"
+              required
+              autoComplete="name"
+              placeholder="이름"
+              value={joinName}
+              onChange={(e) => {
+                setJoinName(e.target.value);
+              }}
+              className="block w-full h-12 px-3 py-2 text-base text-gray-900 placeholder-gray-400 border border-gray-300 focus:z-10 focus:outline-indigo-600 focus:ring-indigo-600 sm:text-sm rounded-t-md"
+            />
+            <input
+              id="JoinDate"
+              name="JoinDate"
+              type="date"
+              min="1950-01-01"
+              max={dayjs().format("YYYY-MM-DD")} // 오늘 날짜까지
+              required
+              autoComplete="bday"
+              placeholder="생년월일"
+              value={joinDate}
+              onChange={(e) => {
+                setJoinDate(e.target.value);
+              }}
+              className="block w-full h-12 px-3 py-2 text-base text-gray-900 placeholder-gray-400 border border-gray-300 focus:z-10 focus:outline-indigo-600 focus:ring-indigo-600 sm:text-sm border-t-0 rounded-b-md"
+            />
+          </div>
 
           <div>
-            <button type="submit"
-              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" >
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
               회원가입
             </button>
           </div>
         </form>
-
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Join
+export default Join;
